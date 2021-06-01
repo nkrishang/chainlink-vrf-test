ea7565c85e16eba9341d0350ea6ee62ac17bbb88
nkrishang <kn1457@nyu.edu>
2021-06-01 17:36:49 -0400

    Add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+// Chainlink info for Rinkeby
+
+const vrfCoordinator = '0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B'
+const linkTokenAddress = '0x01be23585060835e02b77ef475b0cc51aa1e0709'
+const keyHash = '0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311'
+
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+  
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Factory = await ethers.getContractFactory("RandomEmitter");
+  const randomEmitter = await Factory.deploy(vrfCoordinator, linkTokenAddress, keyHash);
+
+  console.log("Pack ERC1155 token address:", randomEmitter.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// Random Emitter address -- 
